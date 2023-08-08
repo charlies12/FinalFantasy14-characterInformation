8f4a0ab4d6fc5a9c316ce95685932677d14bffd9
Charlie <charlie.sanchez31@gmail.com>
2023-08-08 14:51:29 -0700

    Added text file Sopie.txt as an example. Added method get_name to class Avatar that just returns character's name. Method get_basic_info in Avatar class returns name, gender, race, role, and class. Function add_to_page now uses character name as file name.

--- basic_attributes.py
+++ basic_attributes.py
@@ -1,3 +1,8 @@
+"""I plan on taking some information from my personal ff14 playable character and just trying to see all the stats
+   based on different job classes. Write them to a file and just analyze the information."""
+
+
+# Avatar class is supposed to create a character which we will fill up with information
 class Avatar:
     def __init__(self, name, gender, race, role, character_class):
         self.name = name
@@ -6,14 +11,33 @@ class Avatar:
         self.role = role
         self.character_class = character_class
 
-    def __str__(self):
+    def get_name(self):
+        return self.name
+
+    def get_basic_info(self):
         return (f"Name: {self.name}\n"
                 f"Gender: {self.gender}\n"
                 f"Race: {self.race}\n"
                 f"Role: {self.role}\n"
-                f"Class: {self.character_class}")
+                f"Class: {self.character_class}\n")
+        # info_dictionary = {"Name": self.name,
+        #                    "Gender": self.gender,
+        #                    "Race": self.race,
+        #                    "Role": self.role,
+        #                    "Class": self.character_class
+        #                    }
+        # return info_dictionary
 
 
+    # def __str__(self):
+    #     return (f"Name: {self.name}\n"
+    #             f"Gender: {self.gender}\n"
+    #             f"Race: {self.race}\n"
+    #             f"Role: {self.role}\n"
+    #             f"Class: {self.character_class}")
+
+
+# Weapon class creates a weapon that accepts a weapon name, weapon class and item level
 class Weapon:
     def __init__(self, weapon_name, weapon_class, weapon_item_level):
         self.weapon_name = weapon_name
@@ -21,6 +45,8 @@ class Weapon:
         self.weapon_item_level = weapon_item_level
 
 
+# Since there are basically 2 different types of combat weapon types. Class Magical still needs the previous attributes
+# but we will be adding magical damage here.
 class MagicalWeapon(Weapon):
     def __init__(self, weapon_name, weapon_class, weapon_item_level, magical_damage):
         super().__init__(weapon_name, weapon_class, weapon_item_level)
@@ -33,6 +59,7 @@ class MagicalWeapon(Weapon):
                 f"Magic Damage: {self.magical_damage}\n")
 
 
+# PhysicalWeapon is like MagicalWeapon that only the type of damage differs and will be labeled as physical damage
 class PhysicalWeapon(Weapon):
     def __init__(self, weapon_name, weapon_class, weapon_item_level, physical_damage):
         super().__init__(weapon_name, weapon_class, weapon_item_level)
@@ -45,12 +72,20 @@ class PhysicalWeapon(Weapon):
                 f"Physical Damage: {self.physical_damage}\n")
 
 
-sophie = Avatar("Sophie", "Female", "Midlander", "Healer", "White Mage")
+sophie = Avatar("Sophie than", "Female", "Midlander", "Healer", "White Mage")
 knee_height = Avatar("Mountain", "Male", "Lalafell", "Tank", "Dark Knight")
 
 dark_knight_weapon = PhysicalWeapon("Shadowbringer", "Dark Knight", 530, 90)
 white_mage_weapon = MagicalWeapon("Staff", "White Mage", 1, 20)
-print(sophie)
-print(white_mage_weapon)
-print(knee_height)
-print(dark_knight_weapon)
+
+
+def add_to_page(info):
+    with open(f"{sophie.get_name()}.txt", "a+") as my_character_info:
+        my_character_info.write(info)
+
+
+add_to_page(sophie.get_basic_info())
+# print(sophie.get_basic_info())
+# print(white_mage_weapon)
+# print(knee_height)
+# print(dark_knight_weapon)
